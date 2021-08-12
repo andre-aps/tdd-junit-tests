c44f4b8c0b6ffade8b98a42bfff05aac4bd72e25
André <andre.pereira@mportal.com.br>
2021-08-12 20:27:12 -0300

    implementing readjustment functionality

---
+++ src/main/java/br/com/alura/tdd/service/ReajusteService.java
@@ -0,0 +1,15 @@
+package br.com.alura.tdd.service;
+
+import java.math.BigDecimal;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class ReajusteService {
+
+	public void concedeReajuste(Funcionario funcionario, Desempenho desempenho) {
+		BigDecimal percentual = desempenho.percentualReajuste();
+		funcionario.reajustaSalario(funcionario.getSalario().multiply(percentual));
+	}
+
+}
