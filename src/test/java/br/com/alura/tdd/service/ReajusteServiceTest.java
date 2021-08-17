674e053171ee252097364e49a4987b1b656e29bd
André <andre.pereira@mportal.com.br>
2021-08-16 21:10:46 -0300

    testing whether the salary has been readjusted to three percent

---
+++ src/test/java/br/com/alura/tdd/service/ReajusteServiceTest.java
@@ -0,0 +1,34 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Desempenho;
+import br.com.alura.tdd.modelo.Funcionario;
+
+class ReajusteServiceTest {
+	
+	private Funcionario funcionario;
+	private ReajusteService reajusteService;
+
+	@BeforeEach
+	void setup() {
+		funcionario = new Funcionario("João", LocalDate.now(), new BigDecimal("1000.00"));
+		reajusteService = new ReajusteService();
+	}
+
+	@Test
+	void mustReadjustTheSalaryIfTheReadjustmentIsThreePercent() {
+		//when
+		reajusteService.concedeReajuste(funcionario, Desempenho.A_DESEJAR);
+	
+		//then
+		assertEquals(new BigDecimal("1030.00"), funcionario.getSalario());
+	}
+	
+}
