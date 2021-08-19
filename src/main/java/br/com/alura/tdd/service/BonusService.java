3a44461d9275231574300c177e8a82f92e3f0413
André <andre.gmaps@gmail.com>
2021-08-19 20:22:20 -0300

    flagging exception in calcularBonus method

--- src/main/java/br/com/alura/tdd/service/BonusService.java
+++ src/main/java/br/com/alura/tdd/service/BonusService.java
@@ -3,14 +3,15 @@ package br.com.alura.tdd.service;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 
+import br.com.alura.tdd.exception.BonusException;
 import br.com.alura.tdd.modelo.Funcionario;
 
 public class BonusService {
 
-	public BigDecimal calcularBonus(Funcionario funcionario) {
+	public BigDecimal calcularBonus(Funcionario funcionario) throws BonusException {
 		BigDecimal bonus = funcionario.getSalario().multiply(new BigDecimal("0.1"));
 		if (bonus.compareTo(new BigDecimal("1000")) > 0) {
-			bonus = BigDecimal.ZERO;
+			throw new BonusException("Bônus superior à R$ 1.000,00");
 		}
 		return bonus.setScale(2, RoundingMode.HALF_UP);
 	}
