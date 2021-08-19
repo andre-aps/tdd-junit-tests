eab6a53707a843f25657aeaa6239836f2f8040a4
André <andre.gmaps@gmail.com>
2021-08-19 20:19:25 -0300

    creating BonusException

---
+++ src/main/java/br/com/alura/tdd/exception/BonusException.java
@@ -0,0 +1,11 @@
+package br.com.alura.tdd.exception;
+
+public class BonusException extends Exception {
+
+	private static final long serialVersionUID = 1L;
+	
+	public BonusException(String message) {
+		super(message);
+	}
+
+}
